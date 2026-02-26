1d5149d25df2c317566a61642342550ba6c3e5f5
Edmond <ejgmstassen@gmail.com>
2026-02-26 20:46:44 +0100

    Add news-fetch-install.sh with community-scripts framework

--- news-fetch-install.sh
+++ news-fetch-install.sh
@@ -5,13 +5,57 @@
 # License: MIT
 # Source: https://github.com/EdmondStassen/proxmox-scripts
 
-source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts
+# Author: Edmond Stassen
+# License: MIT
+# Source: https://github.com/EdmondStassen/proxmox-scripts
+
+APP="news-fetch"
+var_tags="${var_tags:-docker;newsletter;news_fetch}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-1024}"
+var_disk="${var_disk:-10}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
 color
-verb_ip6
 catch_errors
-setting_up_container
-network_check
-update_os
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  
+  msg_info "Updating Container OS"
+  $STD apt-get update
+  $STD apt-get -y upgrade
+  msg_ok "Updated Container OS"
+  
+  msg_info "Updating Docker"
+  $STD apt-get install --only-upgrade -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  msg_ok "Updated Docker"
+  
+  PROJECT_DIR="/opt/news_fetch"
+  if [[ -d "$PROJECT_DIR" ]]; then
+    msg_info "Updating news_fetch repository"
+    cd "$PROJECT_DIR"
+    $STD git pull
+    $STD docker compose down
+    $STD docker compose up -d
+    msg_ok "Updated news_fetch"
+  fi
+  
+  msg_ok "Update completed"
+  exit
+}
+
+start
+build_container
+description
 
 msg_info "Updating Container OS"
 $STD apt-get update
